3b05e1511c93ff5330e90d6c9f0980dc76b840f5
pawan.singh <pawan.singh@digitaslbi.com>
2017-07-27 20:45:55 +0530

    added blobcontext

--- Azure.Web/Implementation/Abstract/IContext.cs
+++ Azure.Web/Implementation/Abstract/IContext.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
 
 namespace Azure.Web.Implementation.Abstract
 {
     interface IContext
     {
-        dynamic Context { get; }
+        CloudStorageAccount CloudStorageAccount { get; }
         void Add();
 
     }
--- Azure.Web/Implementation/Concrete/BlobContext.cs
+++ Azure.Web/Implementation/Concrete/BlobContext.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Web;
 using Azure.Web.Implementation.Abstract;
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace Azure.Web.Implementation.Concrete
 {
-    public class SqlContext : IContext
+    public class BlobContext : IContext
     {
         public CloudStorageAccount CloudStorageAccount
         {
@@ -18,6 +19,11 @@ namespace Azure.Web.Implementation.Concrete
                 return CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=pwn22072017;AccountKey=yoDRGiE0aVgLQAZrJaz4XZY+Gt+rNaaxoaL+0C+eKEFZC8DG+dzJdJLjHHJOCWzev28Y7WhMAZcbRzP7lGr4fg==;EndpointSuffix=core.windows.net");
 
             }
+        }
+
+        public CloudBlobClient BlobClient
+        {
+            get { return CloudStorageAccount.CreateCloudBlobClient(); }
         }
 
         public void Add()
