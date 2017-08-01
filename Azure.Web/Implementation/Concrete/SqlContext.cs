104c955bcda6b229536052b46879dc991d9718f4
pawan.singh <pawan.singh@digitaslbi.com>
2017-08-01 17:03:48 +0530

    init commit

--- Azure.Web/Implementation/Concrete/SqlContext.cs
+++ Azure.Web/Implementation/Concrete/SqlContext.cs
@@ -9,6 +9,9 @@ namespace Azure.Web.Implementation.Concrete
 {
     public class SqlContext : IContext
     {
+        private string connstr =
+            @"Server=tcp:server1111111.database.windows.net,1433;Initial Catalog=DB1;Persist Security Info=False;User ID=pwnusr;Password=pass123!@#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
         public CloudStorageAccount CloudStorageAccount
         {
             get
