576ceddfed709b55e12bc597139d8bfcd507e986
pawan.singh <pwn.singh14@gmail.com>
2017-08-06 14:32:50 +0100

    init commit

---
+++ Azure.Web/Controllers/BlobDemoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Azure.Web.Controllers
+{
+    public class BlobDemoController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            //var products = db.Products.Include(p => p.ProductCategory).Include(p => p.ProductModel);
+            return View();
+        }
+
+        public async Task<ActionResult> Add()
+        {
+            //var products = db.Products.Include(p => p.ProductCategory).Include(p => p.ProductModel);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Images/"), fileName);
+                    file.SaveAs(path);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+    }
+}
---
+++ Azure.Web/Controllers/TableDemoController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Azure.Web.Controllers
+{
+    public class TableDemoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
