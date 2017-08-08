921c9417c2cdbfdae60af6fc73ec0939327604ec
pawan.singh <pawan.singh@digitaslbi.com>
2017-08-08 20:02:23 +0530

    init commit

---
+++ Azure.Web/Helper/TableHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table.DataServices;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Azure.Web.Helper
+{
+    public static class TableHelper
+    {
+        private static string CloudStorageString = "DefaultEndpointsProtocol=https;AccountName=r3storageaccount;AccountKey=lon2jmQKYv3e3T6rXFNvgZw5HSuVnmlNpJYXH2uRYV++AjMxjYqa+9e0wMZgii+O72ERI0zabk9F61viHrIaxw==;EndpointSuffix=core.windows.net";
+
+        public static CloudStorageAccount CloudStorageAccount
+        {
+            get
+            {
+                //return CloudStorageAccount.DevelopmentStorageAccount;
+                return CloudStorageAccount.Parse(CloudStorageString);
+            }
+        }
+
+        public static CloudTableClient CloudTableServiceClient
+        {
+            get
+            {
+                return CloudStorageAccount.CreateCloudTableClient();
+            }
+        }
+
+        public static CloudTable GetTableReference(string name)
+        {
+            var reference = CloudTableServiceClient.GetTableReference(name);
+            reference.CreateIfNotExists(new TableRequestOptions
+            {
+
+            });
+            return reference;
+        }
+    }
+}
