bac77e5ab9e1d546eb3fcb0ab5cfc6ccaa5f4f46
pawan.singh <pawan.singh@digitaslbi.com>
2017-08-09 16:50:48 +0530

    init commit

--- Azure.Web/Controllers/TableDemoController.cs
+++ Azure.Web/Controllers/TableDemoController.cs
@@ -1,12 +1,30 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Azure.Web.Models;
 
 namespace Azure.Web.Controllers
 {
     public class TableDemoController : Controller
     {
+        private static List<StudentEntity> _studentlist = null;
         public ActionResult Index()
         {
+            var model = GetDummyList();
+
             return View();
+        }
+
+        public static List<StudentEntity> GetDummyList()
+        {
+            return _studentlist = new List<StudentEntity>
+            {
+               new StudentEntity
+               {
+                  Name = "Ram",
+                  Std ="Class1",
+                  RollNo = 54
+               }
+            };
         }
     }
 }
