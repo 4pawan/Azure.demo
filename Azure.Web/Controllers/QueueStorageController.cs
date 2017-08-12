3e361df500d8a914ce315d4af72e66f520dd9ea3
pawan.singh <pwn.singh14@gmail.com>
2017-08-12 15:52:01 +0100

    init commit

---
+++ Azure.Web/Controllers/QueueStorageController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Azure.Web.Helper;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace Azure.Web.Controllers
+{
+    public class QueueStorageController : Controller
+    {
+
+        private string QueueName = "q11";
+
+        private CloudQueue QueueReference
+        {
+            get { return QueueStorageHelper.GetQueueReference(QueueName); }
+        }
+
+
+
+        // GET: QueueStorage
+        public ActionResult Index()
+        {
+            var model = QueueStorageHelper.ReadMsgFromQueue(QueueReference);
+
+            return View();
+        }
+
+        // GET: QueueStorage/Create
+        public ActionResult Create()
+        {
+            var msg = QueueStorageHelper.RandomString(13);
+            QueueStorageHelper.SendMsgToQueue(QueueReference, msg);
+            return View();
+        }
+    }
+}
---
+++ Azure.Web/Helper/QueueStorageHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Azure.Web.Helper
+{
+    public class QueueStorageHelper
+    {
+        private static string CloudStorageString = "DefaultEndpointsProtocol=https;AccountName=pwnstorageaccount;AccountKey=gs8+19zH26niBDI4eMN0l94XylNLCxFOIXwmixQYnH7geFdke9Y2yyWmDK8RZTlyDvD2L4lk3uQYnXmsElvxpQ==;EndpointSuffix=core.windows.net";
+        private static readonly Random random = new Random();
+
+        public static CloudStorageAccount CloudStorageAccount
+        {
+            get
+            {
+                //return CloudStorageAccount.DevelopmentStorageAccount;
+                return CloudStorageAccount.Parse(CloudStorageString);
+            }
+        }
+
+        public static CloudQueueClient CloudQueueServiceClient
+        {
+            get
+            {
+                return CloudStorageAccount.CreateCloudQueueClient();
+            }
+        }
+
+        public static CloudQueue GetQueueReference(string name)
+        {
+            var reference = CloudQueueServiceClient.GetQueueReference(name);
+            reference.CreateIfNotExists();
+            return reference;
+        }
+
+        public static void SendMsgToQueue(CloudQueue reference, string msg)
+        {
+            reference.AddMessage(new CloudQueueMessage(msg), TimeSpan.FromHours(3));
+        }
+        public static CloudQueueMessage ReadMsgFromQueue(CloudQueue reference)
+        {
+            var msg = reference.GetMessage();
+            reference.DeleteMessage(msg);
+            return msg;
+        }
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
