959684b2500f0730dcd097fd351bad1c6b21f8f1
pawan.singh <pwn.singh14@gmail.com>
2017-08-15 18:12:48 +0100

    init commit

---
+++ Azure.Web/Controllers/TopicsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Azure.Web.Controllers
+{
+    public class TopicsController : Controller
+    {
+        // GET: Topics
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Topics/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Azure.Web/Helper/TopicsServiceBusHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace Azure.Web.Helper
+{
+    public static class TopicsServiceBusHelper
+    {
+        private static string ServiceBusConnString = "Endpoint=sb://pwnsb11.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=xypMiV2LnRk+Y9KGih2x4SPHatsDbIC0R0bT7jZBhOU=";
+        private static string TopicName = "tp1";
+        private static string SubscriberName = "sb1";
+        private static readonly Random random = new Random();
+
+        public static TopicClient TopicClient
+        {
+            get
+            {
+                return TopicClient.CreateFromConnectionString(ServiceBusConnString,TopicName);
+            }
+        }
+
+        public static SubscriptionClient SubscriptionClient
+        {
+            get { return SubscriptionClient.CreateFromConnectionString(ServiceBusConnString,TopicName,SubscriberName); }
+        }
+
+        public static void SendMsgToTopics(string msg)
+        {
+            TopicClient.Send(new BrokeredMessage(msg));
+        }
+        public static BrokeredMessage ReadMsgFromTopics(CloudQueue reference)
+        {
+            BrokeredMessage msg = SubscriptionClient.Receive(Int64.MaxValue);
+            return msg;
+        }
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
